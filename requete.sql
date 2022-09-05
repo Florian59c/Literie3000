d7e4a625a013facd3ead22c10806ed33d6365e8e
Florian Cagnon <florian.cagnon@live.fr>
2022-09-05 11:24:53 +0200

    modification de la bdd et ajout de données

--- requete.sql
+++ requete.sql
@@ -4,17 +4,16 @@ USE literie3000;
 
 CREATE TABLE lit (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    nom VARCHAR(100) NOT NULL,
-    id_marque INT NOT NULL,
-    id_dimension INT NOT NULL,
-    prix DECIMAL NOT NULL,
-    prix_reduit DECIMAL,
-    FOREIGN KEY (id_marque) REFERENCES marque(id)
+    id_marque INT,
+    nom VARCHAR(100),
+    image VARCHAR(255) NOT NULL,
+    prix DECIMAL(6,2) NOT NULL,
+    prix_reduit DECIMAL(6,2)
 );
 
 CREATE TABLE marque (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    nom_marque VARCHAR(50) NOT NULL
+    nom_marque VARCHAR(50) NOT null
 );
 
 CREATE TABLE dimension (
@@ -22,10 +21,56 @@ CREATE TABLE dimension (
     taille VARCHAR(10) NOT NULL
 );
 
-CREATE TABLE lit_dimaension (
+CREATE TABLE lit_dimension (
     id_lit INT,
     id_dimension INT,
     PRIMARY KEY (id_lit, id_dimension),
     FOREIGN KEY (id_lit) REFERENCES lit(id),
     FOREIGN KEY (id_dimension) REFERENCES dimension(id)
 );
+
+ALTER TABLE lit
+ADD FOREIGN KEY (id_marque)
+REFERENCES marque(id);
+
+INSERT INTO marque 
+(nom_marque)
+VALUES 
+("Epeda"),
+("Dreamway"),
+("Bultex"),
+("Dorsoline"),
+("MemoryLine");
+
+INSERT INTO dimension 
+(taille)
+VALUES 
+("90x190"),
+("140x190"),
+("160x200"),
+("180x200"),
+("200x200");
+
+INSERT INTO lit (
+    id_marque, nom, image, prix, prix_reduit
+)
+VALUES (
+    1, "Matelas Isamel", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsO1agg_G3N95gtpQdyR7dNfpDob5rhYuhBA&usqp=CAU", 759.00, 529.00 
+),
+(
+    2, "Matelas José", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2quQc32W_G4zNmKooYHisXvP7F9ofCQFmcQ&usqp=CAU", 809.00, 709.00 
+),
+(
+    3, "Matelas VMC", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwuOlUr6DoqCRcz5UET0HDvvylgJYaVAWFtg&usqp=CAU", 759.00, 529.00 
+),
+(
+    4, "Matelas Jeanne", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6QQICeZcJBJlhaOm3o34x7U4RRHscd6L8LA&usqp=CAU", 1019.00, 509.00 
+);
+
+INSERT INTO lit_dimension 
+(id_lit, id_dimension)
+VALUES 
+(1, 1),
+(2, 1),
+(3, 2),
+(4, 3);
